ffca48a095399734c7c3bac7208b75907408e637
Md. Ariful ISlam <arifewucse2016@gmail.com>
2023-03-01 10:40:35 +0600

    initial

---
+++ app/src/main/java/com/cit/designpatternbykotlin/HomeFragment.kt
@@ -0,0 +1,33 @@
+package com.cit.designpatternbykotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.cit.designpatternbykotlin.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+    lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.logoutBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_loginFragment2)
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/cit/designpatternbykotlin/LoginFragment.kt
@@ -0,0 +1,34 @@
+package com.cit.designpatternbykotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.cit.designpatternbykotlin.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+   lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentLoginBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+        }
+    }
+
+
+}
